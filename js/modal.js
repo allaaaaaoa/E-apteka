7e016bbb6de80929a903b2890e4e930b41e0693f
AllaSn <you@example.com>
2022-05-16 23:00:59 +0300

    modal window

--- js/modal.js
+++ js/modal.js
@@ -35,8 +35,6 @@ modalBtn1.addEventListener('click' , (e) => {
 })
 
 
-
-
 const closeCity = document.querySelector('.modalCity')
 
 
@@ -61,8 +59,97 @@ modalBtn4.addEventListener('click' , (e) => {
 })
 
 const closeCabinet = document.querySelector('.close-cabinet')
-console.log(closeCabinet);
+
 
 closeCabinet.addEventListener('click' , (e) => {
     modalCabinet.style.display = "none";
 })
+
+
+
+///modal-email///
+
+
+const modalEmail = document.querySelector('.modal-email')
+
+
+const telTextEmail = document.querySelector('.tel-text-email')
+
+
+telTextEmail.addEventListener('click' , (e) => {
+    modalEmail.style.display === "flex" ? modalEmail.style.display = "none" : modalEmail.style.display = "flex";
+})
+
+const closeEmail = document.querySelector('.close-email')
+
+
+///modal-tel///
+
+
+const modalTel = document.querySelector('.modal-tel')
+
+
+const telTextTel = document.querySelector('.tel-text-tel')
+
+
+
+telTextTel.addEventListener('click' , (e) => {
+    modalTel.style.display === "flex" ? modalTel.style.display = "none" : modalTel.style.display = "flex";
+})
+
+
+const closeTel = document.querySelector('.close-tel')
+
+
+///modal-tel2///
+
+
+const modalTel2 = document.querySelector('.modal-tel2')
+
+
+const telTextTel2 = document.querySelector('.tel-text-tel2')
+
+
+
+telTextTel2.addEventListener('click' , (e) => {
+    modalTel2.style.display === "flex" ? modalTel2.style.display = "none" : modalTel2.style.display = "flex";
+})
+
+
+const closeTel2 = document.querySelector('.close-tel2')
+
+
+
+///modal-button-call///
+
+
+const modalButtonCall = document.querySelector('.modal-button-call')
+
+
+const modalButtonHeader = document.querySelector('.modal-button-header')
+
+
+
+modalButtonHeader.addEventListener('click' , (e) => {
+    modalButtonCall.style.display === "flex" ? modalButtonCall.style.display = "none" : modalButtonCall.style.display = "flex";
+})
+
+const closeCall = document.querySelector('.close-call')
+
+
+
+///modal-button-footer///
+
+
+const modalButFooter = document.querySelector('.modal-but-footer')
+
+
+const modalButtonFooter = document.querySelector('.modal-button-footer')
+
+
+ 
+modalButtonFooter.addEventListener('click' , (e) => {
+    modalButFooter.style.display === "flex" ? modalButFooter.style.display = "none" : modalButFooter.style.display = "flex";
+})
+
+const closeFoot = document.querySelector('.close-foot')
